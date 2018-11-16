a71df91e6ae1de2cc9dcd849521ec6caf4b291c1
kat <sarah.kathryn.peters@gmail.com>
2018-11-15 16:32:13 -0800

    Add routes for bands

--- music_project/config/routes.rb
+++ music_project/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:create, :destroy, :new, :show]
-  resource :session, only: [:create, :new, :destroy]
+  resources :session, only: [:create, :new, :destroy]
 
-  # resources :bands, only: [:index, :edit, :create, :new, :show, :update, :destroy]
+  resources :bands, only: [:create, :new, :show, :index, :edit, :update, :destroy]
+end
