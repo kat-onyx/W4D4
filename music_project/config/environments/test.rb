55b6b425b4e6989075ee09e16c9516b823f9424c
kat <sarah.kathryn.peters@gmail.com>
2018-11-15 20:21:40 -0800

    Set up files for homework/testing

--- music_project/config/environments/test.rb
+++ music_project/config/environments/test.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+  # Configure default mail server
+  Rails.application.routes.default_url_options[:host] = 'domain.com'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
