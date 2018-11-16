516b7bcd1f73fcb2ff936b9399a6c4157585fc6a
kat <sarah.kathryn.peters@gmail.com>
2018-11-15 22:05:39 -0800

    Add some controller tests

---
+++ music_project/spec/controllers/users_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, :type => :controller do
+
+  describe "GET #new" do
+    it "renders the new template" do
+      get :new, {}
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "POST #create" do
+    context "with invalid params" do
+      it "validates the presence of the user's email and password" do
+        post :create, params: { user: { email: 'derp@g.com', password: ''}}
+      end
+      it "validates that the password is at least 6 characters long" do
+        post :create, params: { user: { email: 'derp@g.com', password: '12345'}}
+        expect(response).to render_template(:new)
+        expect(flash[:errors]).to be_present
+      end
+    end
+
+    context "with valid params" do
+      it "redirects user to bands index on success"
+    end
+  end
+end
