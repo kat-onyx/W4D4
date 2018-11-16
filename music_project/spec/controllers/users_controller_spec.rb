174371d0cfef986a5cbf4332bbf8f20e9c6e7cb0
kat <sarah.kathryn.peters@gmail.com>
2018-11-15 22:12:48 -0800

    Complete last valid params test for users_controller

--- music_project/spec/controllers/users_controller_spec.rb
+++ music_project/spec/controllers/users_controller_spec.rb
@@ -12,17 +12,20 @@ RSpec.describe UsersController, :type => :controller do
   describe "POST #create" do
     context "with invalid params" do
       it "validates the presence of the user's email and password" do
-        post :create, params: { user: { email: 'derp@g.com', password: ''}}
+        post :create, params: { users: { email: 'derp@g.com', password: ''}}
       end
       it "validates that the password is at least 6 characters long" do
-        post :create, params: { user: { email: 'derp@g.com', password: '12345'}}
+        post :create, params: { users: { email: 'derp@g.com', password: '12345'}}
         expect(response).to render_template(:new)
         expect(flash[:errors]).to be_present
       end
     end
 
     context "with valid params" do
-      it "redirects user to bands index on success"
+      it "redirects user to bands index on success" do
+        post :create, params: { users: {email: 'derp@g.com', password: '123456'}}
+        expect(response).to redirect_to(user_url(User.find_by(email: 'derp@g.com')))
+      end
     end
   end
 end
