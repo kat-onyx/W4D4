96f0672c1aecc19a7dc9c17f1f8e31b17bc86dd2
kat <sarah.kathryn.peters@gmail.com>
2018-11-15 21:28:14 -0800

    Add validations for User model

--- music_project/spec/models/user_spec.rb
+++ music_project/spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { User.new(email: 'lol', password: '123456')}
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest)}
+  it { should validate_length_of(:password).is_at_least(6)}
 end
